c243867a576e1710e527ae80aa4ed75ce4826218
Jay Stakelon <stakelon@gmail.com>
2015-03-12 21:41:00 -0700

    update storyboard colors and fonts

--- Frameless/AppDefaults.swift
+++ Frameless/AppDefaults.swift
@@ -19,3 +19,6 @@ let FRAMER_BLUE = UIColorFromHex(0x28AFFA)
 let FRAMER_GREEN = UIColorFromHex(0x7DDC16)
 let FRAMER_PURPLE = UIColorFromHex(0x9178E2)
 let FRAMER_CYAN = UIColorFromHex(0x2DD7AA)
+
+let FRAMER_TEXT = UIColorFromHex(0x24262A)
+let FRAMER_LIGHT_TEXT = UIColorFromHex(0xAEB2BA)
